f3d3a5b300f19f687b171112d658a07caa78089c
qbl <farabi.iqbal@gmail.com>
2018-12-28 00:00:35 +0700

    Refactored repeat func to use repeatCount constant

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,9 +1,11 @@
 package iteration
 
+const repeatCount = 5
+
 // Repeat takes a character and repeats it five times
 func Repeat(character string) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < repeatCount; i++ {
 		repeated += character
 	}
 
