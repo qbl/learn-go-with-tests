6a4b5feb36abcb265df83a2c39f0102a47f2f211
qbl <farabi.iqbal@gmail.com>
2018-12-28 00:03:05 +0700

    Added test benchmark

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -10,3 +10,9 @@ func TestRepeat(t *testing.T) {
 		t.Errorf("expected: %s; actual: %s", expected, actual)
 	}
 }
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
+}
