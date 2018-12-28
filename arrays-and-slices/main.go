d0d49ea576c3aeeb2f50116f5e29cf367b4f73fd
qbl <farabi.iqbal@gmail.com>
2018-12-28 10:23:34 +0700

    Changed sum to accepts slice instead of array

--- arrays-and-slices/main.go
+++ arrays-and-slices/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 // Sum accepts an array of numbers and return the sum of its elements
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
@@ -13,6 +13,6 @@ func Sum(numbers [5]int) int {
 }
 
 func main() {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	numbers := []int{1, 2, 3, 4, 5}
 	fmt.Println(Sum(numbers))
 }
--- arrays-and-slices/sum_test.go
+++ arrays-and-slices/sum_test.go
@@ -3,12 +3,25 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("Sum of five numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	actual := Sum(numbers)
-	expected := 15
+		actual := Sum(numbers)
+		expected := 15
 
-	if actual != expected {
-		t.Errorf("actual: %d; expected: %d", actual, expected)
-	}
+		if actual != expected {
+			t.Errorf("actual: %d; expected: %d", actual, expected)
+		}
+	})
+
+	t.Run("Sum of three numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+
+		actual := Sum(numbers)
+		expected := 15
+
+		if actual != expected {
+			t.Errorf("actual: %d; expected: %d", actual, expected)
+		}
+	})
 }
