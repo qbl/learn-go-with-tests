6b4758decf165250bfbbe17cd44576c49131427f
qbl <farabi.iqbal@gmail.com>
2018-12-28 09:49:12 +0700

    Changed repeat func to accept count

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,11 +1,9 @@
 package iteration
 
-const repeatCount = 5
-
 // Repeat takes a character and repeats it five times
-func Repeat(character string) string {
+func Repeat(character string, count int) string {
 	var repeated string
-	for i := 0; i < repeatCount; i++ {
+	for i := 0; i < count; i++ {
 		repeated += character
 	}
 
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -3,7 +3,7 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	actual := Repeat("a")
+	actual := Repeat("a", 5)
 	expected := "aaaaa"
 
 	if actual != expected {
@@ -13,6 +13,6 @@ func TestRepeat(t *testing.T) {
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 5)
 	}
 }
