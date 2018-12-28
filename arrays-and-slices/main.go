afe17824d434cf5b065cc41ce3e5a58198da9378
qbl <farabi.iqbal@gmail.com>
2018-12-28 10:02:05 +0700

    Refactored sum loop to use range

--- arrays-and-slices/main.go
+++ arrays-and-slices/main.go
@@ -5,8 +5,8 @@ import "fmt"
 // Sum accepts an array of numbers and return the sum of its elements
 func Sum(numbers [5]int) int {
 	sum := 0
-	for i := 0; i < len(numbers); i++ {
-		sum += numbers[i]
+	for _, number := range numbers {
+		sum += number
 	}
 
 	return sum
