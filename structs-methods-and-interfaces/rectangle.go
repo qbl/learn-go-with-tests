228e1dd0f0df06473ccf0bb27590244602d97122
qbl <farabi.iqbal@gmail.com>
2019-01-01 09:59:11 +0700

    Changed perimeter and area to accept a rectangle struct instead of width and height

--- structs-methods-and-interfaces/rectangle.go
+++ structs-methods-and-interfaces/rectangle.go
@@ -1,11 +1,16 @@
 package rectangle
 
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
 // Perimeter accepts width and height, and returns perimeter of a rectangle
-func Perimeter(width float64, height float64) float64 {
-	return 2 * (width + height)
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
 }
 
 // Area accepts widht and height, and returns area of a rectangle
-func Area(width float64, height float64) float64 {
-	return width * height
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
 }
--- structs-methods-and-interfaces/rectangle_test.go
+++ structs-methods-and-interfaces/rectangle_test.go
@@ -2,8 +2,10 @@ package rectangle
 
 import "testing"
 
+var rectangle = Rectangle{10.0, 10.0}
+
 func TestPerimeter(t *testing.T) {
-	actual := Perimeter(10.0, 10.0)
+	actual := Perimeter(rectangle)
 	expected := 40.0
 
 	if actual != expected {
@@ -12,8 +14,8 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	actual := Area(12.0, 6.0)
-	expected := 72.0
+	actual := Area(rectangle)
+	expected := 100.0
 
 	if actual != expected {
 		t.Errorf("expcted: %f; actual: %f", expected, actual)
