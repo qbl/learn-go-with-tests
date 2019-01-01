fd65ba36832e6a8e427d5e9b9dd873e45496e5fb
qbl <farabi.iqbal@gmail.com>
2019-01-01 09:43:22 +0700

    Refactored unit tests for array and slices

--- arrays-and-slices/sum_test.go
+++ arrays-and-slices/sum_test.go
@@ -5,6 +5,13 @@ import (
 	"testing"
 )
 
+var checkSum = func(t *testing.T, actual, expected []int) {
+	t.Helper()
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("actual: %d; expected: %d", actual, expected)
+	}
+}
+
 func TestSum(t *testing.T) {
 	t.Run("Sum of five numbers", func(t *testing.T) {
 		numbers := []int{1, 2, 3, 4, 5}
@@ -32,28 +39,19 @@ func TestSum(t *testing.T) {
 func TestSumAll(t *testing.T) {
 	actual := SumAll([]int{1, 2}, []int{0, 9})
 	expected := []int{3, 9}
-
-	if !reflect.DeepEqual(actual, expected) {
-		t.Errorf("actual: %d; expected: %d", actual, expected)
-	}
+	checkSum(t, actual, expected)
 }
 
 func TestSumAllTails(t *testing.T) {
 	t.Run("Sum all tails of non-empty slices", func(t *testing.T) {
 		actual := SumAllTails([]int{1, 2}, []int{0, 9})
 		expected := []int{2, 9}
-
-		if !reflect.DeepEqual(actual, expected) {
-			t.Errorf("actual: %d; expected: %d", actual, expected)
-		}
+		checkSum(t, actual, expected)
 	})
 
 	t.Run("Sum all tails of empty slices", func(t *testing.T) {
 		actual := SumAllTails([]int{}, []int{0, 9})
 		expected := []int{0, 9}
-
-		if !reflect.DeepEqual(actual, expected) {
-			t.Errorf("actual: %d; expected: %d", actual, expected)
-		}
+		checkSum(t, actual, expected)
 	})
 }
