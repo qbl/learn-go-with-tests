0b2ccc12b1d613b5abf054512eb1119c69ca15f3
qbl <farabi.iqbal@gmail.com>
2019-01-03 21:21:48 +0700

    Added custom type called Bitcoin

--- pointers-and-errors/wallet.go
+++ pointers-and-errors/wallet.go
@@ -1,16 +1,19 @@
 package wallet
 
+// Bitcoin is a sample custom type based on integer
+type Bitcoin int
+
 // Wallet has balance
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
 // Deposit adds amount to wallet's balance
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
 // Balance returns the balance of wallet
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
