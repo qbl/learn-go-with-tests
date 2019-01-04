e0d9185b5b857c359daf43b39c76c8290d3b2304
qbl <farabi.iqbal@gmail.com>
2019-01-04 22:48:19 +0700

    Added handling of searching for unknown word in dictionary

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,11 +1,21 @@
 package main
 
+import (
+	"errors"
+)
+
 // Dictionary is a custom type with base type a map of string
 type Dictionary map[string]string
 
 // Search accepts a map of string and key to be searched, and returns value of particular key in the map
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+
+	if !ok {
+		return "", errors.New("could not find the word you were looking for")
+	}
+
+	return definition, nil
 }
 
 func main() {}
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -5,9 +5,23 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	actual := dictionary.Search("test")
-	expected := "this is just a test"
-	assertString(t, actual, expected)
+	t.Run("knwon word", func(t *testing.T) {
+		actual, _ := dictionary.Search("test")
+		expected := "this is just a test"
+
+		assertString(t, actual, expected)
+	})
+
+	t.Run("unknwon word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		expected := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error")
+		}
+
+		assertString(t, err.Error(), expected)
+	})
 }
 
 func assertString(t *testing.T, actual, expected string) {
