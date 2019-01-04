71cf3524d4da77190343134c62ea3cb074229159
qbl <farabi.iqbal@gmail.com>
2019-01-04 23:20:01 +0700

    Refactored dictionary errors into a custom type

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,17 +1,20 @@
 package main
 
-import (
-	"errors"
+// Various error types
+const (
+	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
 )
+
+// DictionaryErr is a custom type to describe errors
+type DictionaryErr string
 
 // Dictionary is a custom type with base type a map of string
 type Dictionary map[string]string
 
-// ErrNotFound returned when word is not found in the dictionary
-var (
-	ErrNotFound   = errors.New("could not find the word you were looking for")
-	ErrWordExists = errors.New("cannot add word because it already exists")
-)
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 // Search accepts the word to be searched, and definition of the word in the dictionary
 func (d Dictionary) Search(word string) (string, error) {
