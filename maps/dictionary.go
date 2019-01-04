24dc4c91ae53a8e48e1159c6787305535234a20f
qbl <farabi.iqbal@gmail.com>
2019-01-04 23:15:35 +0700

    Added handling adding existing word to dictionary

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -8,7 +8,10 @@ import (
 type Dictionary map[string]string
 
 // ErrNotFound returned when word is not found in the dictionary
-var ErrNotFound = errors.New("could not find the word you were looking for")
+var (
+	ErrNotFound   = errors.New("could not find the word you were looking for")
+	ErrWordExists = errors.New("cannot add word because it already exists")
+)
 
 // Search accepts the word to be searched, and definition of the word in the dictionary
 func (d Dictionary) Search(word string) (string, error) {
@@ -22,8 +25,19 @@ func (d Dictionary) Search(word string) (string, error) {
 }
 
 // Add accepts word and definition, and adds them to dictionary
-func (d Dictionary) Add(word, definition string) {
-	d[word] = definition
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
 }
 
 func main() {}
