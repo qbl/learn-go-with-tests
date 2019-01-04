4d16c34637613a21243c956ea19060aec93581c6
qbl <farabi.iqbal@gmail.com>
2019-01-04 22:33:09 +0700

    Refactored dictionary test

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -7,7 +7,11 @@ func TestSearch(t *testing.T) {
 
 	actual := Search(dictionary, "test")
 	expected := "this is just a test"
+	assertString(t, actual, expected)
+}
 
+func assertString(t *testing.T, actual, expected string) {
+	t.Helper()
 	if actual != expected {
 		t.Errorf("actual: %s; expected: %s", actual, expected)
 	}
